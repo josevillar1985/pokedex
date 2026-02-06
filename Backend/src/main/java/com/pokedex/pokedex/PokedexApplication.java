e4749804defd3551777fcdd54a16e40947699f24
Jose Villar <josevillar1985@gmail.com>
2026-02-06 15:17:11 +0100

    Fix: forzando escaneo de controladores y actualizando DTO

--- Backend/src/main/java/com/pokedex/pokedex/PokedexApplication.java
+++ Backend/src/main/java/com/pokedex/pokedex/PokedexApplication.java
@@ -2,12 +2,13 @@ package com.pokedex.pokedex;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.pokedex.pokedex") // <--- Fuerza el escaneo aquí
 public class PokedexApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PokedexApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(PokedexApplication.class, args);
+    }
 }
