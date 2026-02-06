55022e4379dd47db7505c1a8e6c41fdcebe3b539
Jose Villar <josevillar1985@gmail.com>
2026-02-06 14:57:51 +0100

    Fix: replaced MySQL LONGTEXT with PostgreSQL TEXT

--- Backend/src/main/java/com/pokedex/pokedex/Model/Pokemon.java
+++ Backend/src/main/java/com/pokedex/pokedex/Model/Pokemon.java
@@ -15,7 +15,8 @@ public class Pokemon {
     private int ataque;
     private int defensa;
 
-    @Column(columnDefinition = "LONGTEXT")
+    // Cambiado de LONGTEXT a TEXT para compatibilidad con PostgreSQL
+    @Column(columnDefinition = "TEXT")
     private String imagen;
 
     public Pokemon() {
@@ -32,68 +33,21 @@ public class Pokemon {
         this.imagen = imagen;
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public int getNumero() {
-        return numero;
-    }
-
-    public void setNumero(int numero) {
-        this.numero = numero;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getTipo() {
-        return tipo;
-    }
-
-    public void setTipo(String tipo) {
-        this.tipo = tipo;
-    }
-
-    public int getHp() {
-        return hp;
-    }
-
-    public void setHp(int hp) {
-        this.hp = hp;
-    }
-
-    public int getAtaque() {
-        return ataque;
-    }
-
-    public void setAtaque(int ataque) {
-        this.ataque = ataque;
-    }
-
-    public int getDefensa() {
-        return defensa;
-    }
-
-    public void setDefensa(int defensa) {
-        this.defensa = defensa;
-    }
-
-    public String getImagen() {
-        return imagen;
-    }
-
-    public void setImagen(String imagen) {
-        this.imagen = imagen;
-    }
-    
+    // ... (el resto de tus getters y setters se mantienen igual)
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public int getNumero() { return numero; }
+    public void setNumero(int numero) { this.numero = numero; }
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+    public String getTipo() { return tipo; }
+    public void setTipo(String tipo) { this.tipo = tipo; }
+    public int getHp() { return hp; }
+    public void setHp(int hp) { this.hp = hp; }
+    public int getAtaque() { return ataque; }
+    public void setAtaque(int ataque) { this.ataque = ataque; }
+    public int getDefensa() { return defensa; }
+    public void setDefensa(int defensa) { this.defensa = defensa; }
+    public String getImagen() { return imagen; }
+    public void setImagen(String imagen) { this.imagen = imagen; }
 }
